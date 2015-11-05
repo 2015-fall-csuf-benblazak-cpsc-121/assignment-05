dde838a1a72c237e6ac744ed72547eb319e14bda
andy21996 <andy21996@gmail.com>
2015-11-05 10:45:15 -0800

    GCD and Fibonacci

--- solution.cpp
+++ solution.cpp
@@ -24,3 +24,46 @@ int main()
 
 	return 0;
 }
+
+GCD
+
+#include "stdafx.h"
+#include <iostream>
+#include <cmath>
+using std::cout;
+using std::endl;
+
+int gcd(int a, int b)
+{
+	a = abs(a);
+	b = abs(b);
+
+	if (a == 0 || b == 0)
+		return a + b;
+	
+	if (a > b)
+		return gcd(a - b, b);
+	else
+		return gcd(a, b - a);
+}
+
+int gcd_iterative(int a, int b)
+{
+	int minNum = a > b ? b : a;
+	int gcd = 1;
+	for (int i = 1; i <= minNum; i++)
+	{
+		if (a % i == 0 && b % i == 0)
+			gcd = i;
+	}
+
+	return gcd;	
+}
+
+int main()
+{
+	cout << gcd(8, 20) << endl;
+	cout << gcd_iterative(8, 20) << endl;
+
+	return 0;
+}
