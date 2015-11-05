e0989f1084ddb0cf5ae6edf561c78ff8247521a2
andy21996 <andy21996@gmail.com>
2015-11-05 10:37:11 -0800

    Fibonacci

--- solution.cpp
+++ solution.cpp
@@ -1,2 +1,26 @@
-// initial commit test
-// second test
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2015 Duy Nguyen <andy21996@csu.fullerton.edu>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+Fibonacci
+
+#include "stdafx.h"
+#include <iostream>
+using std::cout;
+
+int fib(int a)
+{
+	if (a == 1 || a == 2)
+		return 1;
+
+	return fib(a - 1) + fib(a-2);
+}
+
+int main()
+{
+	for (int i = 1; i < 10; i++)
+		cout << fib(i) << " ";
+
+	return 0;
+}
